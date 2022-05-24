72d9eac924f29a22a2111668d1107275be5287f5
Paolo Marcoccia <marcocciapaolo1991@gmail.com>
2022-05-24 12:59:58 +0200

    Add files via upload

---
+++ BigFont.py
@@ -0,0 +1,10 @@
+#This script will increase the size of the font for all the elements of a matplotlib plot
+import matplotlib.pyplot as plt
+
+plt.rc('font', size =22)
+plt.rc('axes', titlesize =22)
+plt.rc('axes', labelsize =22)
+plt.rc('xtick', labelsize =20)
+plt.rc('ytick', labelsize =20)
+plt.rc('legend', fontsize =22)
+plt.rc('figure', titlesize =22)
